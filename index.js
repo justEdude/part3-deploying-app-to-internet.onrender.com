397a9ab1db519b6e0c6138a90ec1002949c3a886
Danton Pretti <Dantonprettibcs@hotmail.com>
2026-09-14 12:49:23 -0400

    meh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 
 let corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://part3-deploying-app-to-internet-onrender.onrender.com',
   optionsSuccessStatus: 200 
 }
 
